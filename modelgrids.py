82f2fab01673424a49e10428751c411fc7a9ade8
lesommer <julien.lesommer@gmail.com>
2016-04-30 17:00:24 +0200

    replace modelgrids.py

---
+++ modelgrids.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+"""modelgrids module.
+Define classes that give acces to model grid metrics and operators
+(e.g. gradients)
+
+This submodule is still under development. See the project
+[wiki page](https://github.com/lesommer/oocgcm/wiki/modelgrids_design)
+
+"""
+
+import numpy as np
+import xarray as xr
