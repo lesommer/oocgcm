b6632fe86297249c879d98f36826b13079b356fc
lesommer <julien.lesommer@gmail.com>
2016-05-02 10:43:34 +0200

    first implementation of horizontal_gradient

--- modelgrids.py
+++ modelgrids.py
@@ -21,21 +21,28 @@ earthrad = 6371229            # mean earth radius (m)
 
 # Maths parameters
 deg2rad = np.pi / 180.
-mod360 = lambda x: np.mod(x+180,360)-180
+mod360 = lambda x: xu.fmod(x+180,360) - 180
 
 #======================== General Purpose Tools ================================
+# TODO : this should probably move to the main module level.
+#
 def return_xarray_dataset(filename,chunks=None):
     """Return an xarray dataset corresponding to filename.
     """
     return xr.open_dataset(filename,chunks=chunks,lock=False)
 
-def get_xarray_dataarray(filename,varname,chunks=None):
+def get_xarray_dataarray(filename,varname,chunks=None,**extra_kwargs):
     """Return a xarray dataarray corresponding to varname in filename.
     """
     ds = return_xarray_dataset(filename,chunks=chunks)
-    return ds[varname]
+    dataarray = ds[varname]
+    for kwargs in extra_kwargs:
+        dataarray.attrs[kwargs] = extra_kwargs[kwargs]
+    return dataarray
 
-#======================= NEMO Specific Material ================================
+#======================= NEMO-Specific Tools ===================================
+# TODO : this section should move to a file dedicated to gcm specific features.
+#
 class variables_holder_for_2d_grid_from_nemo_ogcm:
     """This class create the variables used in generic_2d_grid.
     """
@@ -47,25 +54,58 @@ class variables_holder_for_2d_grid_from_nemo_ogcm:
         self.chunks = chunks
         self.variables = {}
         self._get = get_xarray_dataarray
+        self.define_projection_coordinate()
         self.define_horizontal_metrics()
         self.define_masks()
         self.chunk(chunks=chunks)
 
+    def define_projection_coordinate(self):
+        self.variables["projection_x_coordinate_at_t_location"] = \
+                        self._get(self.coordinate_file,"nav_lon",\
+                        chunks=self.chunks,grid_location='t')
+        self.variables["projection_y_coordinate_at_t_location"] = \
+                        self._get(self.coordinate_file,"nav_lat",\
+                        chunks=self.chunks,grid_location='t')
+
     def define_horizontal_metrics(self):
         self.variables["cell_x_size_at_t_location"] = \
-                       self._get(self.coordinate_file,"e1t",chunks=self.chunks)
+                        self._get(self.coordinate_file,"e1t",\
+                        chunks=self.chunks,grid_location='t')
         self.variables["cell_y_size_at_t_location"] = \
-                       self._get(self.coordinate_file,"e2t",chunks=self.chunks)
+                        self._get(self.coordinate_file,"e2t",\
+                        chunks=self.chunks,grid_location='t')
+        self.variables["cell_x_size_at_u_location"] = \
+                        self._get(self.coordinate_file,"e1u",\
+                        chunks=self.chunks,grid_location='u')
+        self.variables["cell_y_size_at_u_location"] = \
+                        self._get(self.coordinate_file,"e2u",\
+                        chunks=self.chunks,grid_location='u')
+        self.variables["cell_x_size_at_v_location"] = \
+                        self._get(self.coordinate_file,"e1v",\
+                        chunks=self.chunks,grid_location='v')
+        self.variables["cell_y_size_at_v_location"] = \
+                        self._get(self.coordinate_file,"e2v",\
+                        chunks=self.chunks,grid_location='v')
 
     def define_masks(self):
-        self.variables["sea_binary_mask"] = \
+        self.variables["sea_binary_mask_at_t_location"] = \
                       self._get(self.byte_mask_file,"tmask",\
-                                           chunks=self.chunks)[0,0,...]
+                      chunks=self.chunks,grid_location='t')[0,0,...]
+        self.variables["sea_binary_mask_at_u_location"] = \
+                      self._get(self.byte_mask_file,"umask",\
+                      chunks=self.chunks,grid_location='u')[0,0,...]
+        self.variables["sea_binary_mask_at_v_location"] = \
+                      self._get(self.byte_mask_file,"vmask",\
+                      chunks=self.chunks,grid_location='v')[0,0,...]
+        self.variables["sea_binary_mask_at_f_location"] = \
+                      self._get(self.byte_mask_file,"vmask",\
+                      chunks=self.chunks,grid_location='f')[0,0,...]
 
     def chunk(self,chunks=None):
-        for data in self.variables:
+        for dataname in self.variables:
+            data = self.variables[dataname]
             if isinstance(data, xr.DataArray):
-                data = data.chunk(chunks)
+                self.variables[dataname] = data.chunk(chunks)
 
 def nemo_2d_grid(nemo_coordinate_file=None,nemo_byte_mask_file=None,\
                  chunks=None):
@@ -81,37 +121,72 @@ def nemo_2d_grid(nemo_coordinate_file=None,nemo_byte_mask_file=None,\
 #======================== Generic 2D Grid Class ================================
 class generic_2d_grid:
     """Model agnostic grid object, two dimensional version.
+
     This class holds the arrays that describe the grid and
     implements grid related methods including :
-     - vector calculus
-     - differential operators (gradient, divergence, etc...)
-     - spatial integration
+        - vector calculus
+        - differential operators (gradient, divergence, etc...)
+        - spatial integration
+
+    Methods expect and return xarray dataarrays.
+
+    Assume that dimension names ar 'x' and 'y'.
     """
-    # TODO : keep _variables and _methods upt to date.
-    _variables = [\
-        "grid_shape"\
-       ,"sea_binary_mask"
-       ,"cell_x_size"\
-       ,"cell_y_size"\
+    # TODO : keep _variables and _methods up to date.
+    _required_variables = [\
        ]
-    _methods = [\
-        "rechunk"
-       ,"d_i","d_j","m_i","m_j"\
+    _available_methods = [\
+         "rechunk", "__getitem__"\
+        ,"d_i","d_j"\
        ]
     def __init__(self,variables=None,parameters=None):
         """Initialize grid from dictionary of variables.
         """
         self.variables = variables
         self.parameters = parameters
+        self._define_aliases_for_variabless()
+        self.shape = self.variables["sea_binary_mask_at_t_location"].shape
 
-#---------------------------- Chunking -----------------------------------------
+#--------------------- Aliases for DataArrays ----------------------------------
+    def _define_aliases_for_variabless(self):
+        """Define alias for frequently used variables.
+        This mostly follows nemo name conventions and will evolve in the future.
+        """
+        # coordinates
+        self._navlon = self.variables["projection_x_coordinate_at_t_location"]
+        self._navlat = self.variables["projection_y_coordinate_at_t_location"]
+        # metrics
+        self._e1t = self.variables["cell_x_size_at_t_location"]
+        self._e1u = self.variables["cell_x_size_at_u_location"]
+        self._e1v = self.variables["cell_x_size_at_v_location"]
+        self._e2t = self.variables["cell_y_size_at_t_location"]
+        self._e2u = self.variables["cell_y_size_at_u_location"]
+        self._e2v = self.variables["cell_y_size_at_v_location"]
+
+        # masks
+        self._tmask = self.variables["sea_binary_mask_at_t_location"]
+        self._umask = self.variables["sea_binary_mask_at_u_location"]
+        self._vmask = self.variables["sea_binary_mask_at_v_location"]
+        self._fmask = self.variables["sea_binary_mask_at_f_location"]
+
+
+#--------------------- Chunking and Slicing ------------------------------------
     def rechunk(self,chunks=None):
         """Rechunk all the variables defining the grid.
         """
-        for data in self.variables.values():
+        for dataname in self.variables:
+            data = self.variables[dataname]
             if isinstance(data, xr.DataArray):
-                data = data.chunk(chunks)  ## TODO : does not work yet.
+                self.variables[dataname] = data.chunk(chunks)
 
+    def __getitem__(self,item):
+        """Return a grid object restricted to a subdomain.
+        """
+        sliced_variables = {}
+        for dataname in self.variables:
+            sliced_variables[dataname] = self.variables[dataname][item]
+        return generic_2d_grid(variables=sliced_variables,\
+                             parameters=self.parameters)
 
 #---------------------------- Grid Swapping ------------------------------------
 #- Core swapping utilities
@@ -124,7 +199,7 @@ class generic_2d_grid:
     def norm_of_vector_field(self,a):
         """Return the norm of a vector field a at t-point.
         """
-        return self.scalar_product(a,a)
+        return xu.sqrt(self.scalar_product(a,a))
 
     def scalar_product(self,a,b):
         """Return the scalar product of two vector fields a and b at t-point.
@@ -145,12 +220,15 @@ class generic_2d_grid:
     def d_i(self,q):
         """Return the difference q(i+1) - q(i)
         """
-        pass
+        di = q.shift(x=-1) - q
+        return di
 
     def d_j(self,q):
         """Return the difference q(j+1) - q(j)
         """
-        pass
+        dj = q.shift(y=-1) - q
+        return dj
+
 
     def m_i(self,q):
         """Return the average of q(i+1) and q(i)
@@ -167,7 +245,13 @@ class generic_2d_grid:
         """
         Return the horizontal gradient of a scalar field.
         """
-        pass
+        # TODO : test that the input is a xarray dataarray
+        # TODO : test the input grid location
+        # TODO : check that the chunks are aligned
+        gx = self.d_i(q) / self._e1u
+        gy = self.d_j(q) / self._e2v
+        # TODO : finalize unit and name of output variables
+        return gx,gy
 
     def vertical_component_of_curl(self,a):
         """Return the vertical component of the curl of a vector field.
