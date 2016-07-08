e27d04d74f39020916dc11d6ba2c9e2cbed38e19
Aurelien @LPO <aurelien.junk@gmail.com>
2016-07-08 14:15:09 +0200

    First shot at implementing an eos (JMD95). Not optimized, need clean up.

--- oocgcm/oceanfuncs/eos/jmd95.py
+++ oocgcm/oceanfuncs/eos/jmd95.py
@@ -5,12 +5,293 @@ Equation of state of sea water and related quantities.
 
 This module uses the formulas from Jackett and McDougall (1995) ([1]_).
 
+Potential temperature is the 0-bar reference.
+
+
+Currently, functions are split into two families: one dealing with
+numpy arrays only (if useful for optimisation), the other dealing
+with  xarray dataarrays as input (if no optimisation needed).
+
+The list is as follows:
+
+- Numpy only:
+_density: density from theta0,s,p                        
+
+- Xarray datarray input
+density_meters: density from theta0,s,z with pressure-depth approximation
+                Numpy calculation and output
+_density_xr: density from theta0,s,p
+             Xarray only
+density_meters_xr: density from theta0,s,z with pressure-depth approximation
+                   Xarray only
+
+
+Ckeck:
+>>> density(40,40,10000)
+    1060.93299  
+
+
 References
 ----------
 
-.. [1] Jackett, D.R., Mcdougall, T.J., 1995. Minimal Adjustment of 
-   Hydrographic Profiles to Achieve Static Stability. J. Atmos. 
-   Oceanic Technol. 12, 381–389. 
-
+  .. [1] Jackett, D.R., Mcdougall, T.J., 1995. Minimal Adjustment of 
+     Hydrographic Profiles to Achieve Static Stability. J. Atmos. 
+     Oceanic Technol. 12, 381-389.
 
 """
+
+import numpy as np
+from numba import jit
+
+#from ...core.utils import map_apply, _assert_are_compatible_dataarrays
+from ...core.utils import *
+from matplotlib.pyplot import xkcd
+#from build.lib.oocgcm.parameters.physicalparameters import grav
+
+
+#------------------ Numpy only ---------------------------
+
+
+
+def _density(theta0,S,P):
+    """In-situ density (volumic mass)
+
+    In situ density is computed directly as a function of
+    potential temperature relative to the surface.
+
+    Parameters
+    ----------
+    theta0 : numpy.array
+        potential temperature
+    S : numpy.array
+        salinity
+    P : numpy.array
+        pressure (dB)
+
+    Notes 
+    -----
+    We use  Jackett and McDougall (1995)'s [1]_ equation of state.
+    the in situ density is computed directly as a function of
+    potential temperature relative to the surface (theta0), salt 
+    and pressure
+
+    with:
+
+    - pressure                      p        decibars
+
+    - potential temperature         theta0        deg celsius
+
+    - salinity                      s        psu
+    
+
+    """
+    
+    zsr=np.sqrt(np.abs(S))
+    zt=theta0
+    zs=S
+    zh=P
+     
+    # compute volumic mass pure water at atm pressure
+    zr1= ( ( ( ( 6.536332e-9*zt-1.120083e-6 )*zt+1.001685e-4)*zt
+             -9.095290e-3 )*zt+6.793952e-2 )*zt+999.842594
+    # seawater volumic mass atm pressure
+    zr2= ( ( ( 5.3875e-9*zt-8.2467e-7 ) *zt+7.6438e-5 ) *zt
+           -4.0899e-3 ) *zt+0.824493
+    zr3= ( -1.6546e-6*zt+1.0227e-4 ) *zt-5.72466e-3
+    zr4= 4.8314e-4
+
+    # potential volumic mass (reference to the surface)
+    zrhop= ( zr4*zs + zr3*zsr + zr2 ) *zs + zr1
+
+    # add the compression terms
+    ze = ( -3.508914e-8*zt-1.248266e-8 ) *zt-2.595994e-6
+    zbw= (  1.296821e-6*zt-5.782165e-9 ) *zt+1.045941e-4
+    zb = zbw + ze * zs
+
+    zd = -2.042967e-2
+    zc =   (-7.267926e-5*zt+2.598241e-3 ) *zt+0.1571896
+    zaw= ( ( 5.939910e-6*zt+2.512549e-3 ) *zt-0.1028859 ) *zt - 4.721788
+    za = ( zd*zsr + zc ) *zs + zaw
+
+    zb1=   (-0.1909078*zt+7.390729 ) *zt-55.87545
+    za1= ( ( 2.326469e-3*zt+1.553190)*zt-65.00517 ) *zt+1044.077
+    zkw= ( ( (-1.361629e-4*zt-1.852732e-2 ) *zt-30.41638 )
+           *zt + 2098.925 ) *zt+190925.6
+    zk0= ( zb1*zsr + za1 )*zs + zkw
+
+    prd=  zrhop / (  1.0 - zh / ( zk0 - zh * ( za - zh * zb )))
+
+    return prd
+
+
+
+
+
+#------------------ Wrapped functions for dataarrays ---------------------------
+
+
+
+def density_meters(theta0,s,z):
+    """In-situ density (volumic mass) using depth value
+    in meters. Numpy transposition.
+
+    In situ density is computed directly as a function of
+    potential temperature relative to the surface.
+
+    Parameters
+    ----------
+    theta0 : xarray datarray
+        potential temperature (degC)
+    S : xarray datarray
+        salinity (psu)
+    z : xarray datarray
+        depth (m)
+
+    Returns
+    -------
+    rho : numpy.array
+       density (volumic mass)
+
+    Notes 
+    -----
+   We use  Jackett and McDougall (1995)'s [1]_ equation of state.
+   the in situ density is computed directly as a function of
+   potential temperature relative to the surface (theta0), salt 
+   and pressure (assuming no pressure variation
+   along geopotential surfaces, i.e. the pressure p in decibars
+   is approximated by the depth in meters.
+
+    with:
+
+    - depth                         z        meters
+
+    - temperature                   theta0        deg celsius
+
+    - salinity                      s        psu
+
+
+    """
+
+    theta0=theta0.to_masked_array()
+    s=s.to_masked_array()    
+    z=z.to_masked_array()    
+
+    # caution: depth (m) replaces pressure (db)        
+    rho=_density(theta0,s,z)
+                    
+    return rho
+
+    
+def _density_xr(theta0,S,Z):
+    """In-situ density (volumic mass)
+
+    In situ density is computed directly as a function of
+    potential temperature relative to the surface.
+
+    Parameters
+    ----------
+    theta0 : xarray dataarray
+        potential temperature
+    S : xarray dataarray
+        salinity
+    Z : xarray dataarray
+        pressure (dB)
+
+    Notes 
+    -----
+    We use  Jackett and McDougall (1995)'s [1]_ equation of state.
+    the in situ density is computed directly as a function of
+    potential temperature relative to the surface (theta0), salt 
+    and pressure (assuming no pressure variation
+    along geopotential surfaces, i.e. the pressure p in decibars
+    is approximated by the depth in meters.
+
+    with:
+
+    - pressure                      p        decibars
+
+    - potential temperature         theta0        deg celsius
+
+    - salinity                      s        psu
+    
+
+    """
+    
+    zsr=np.sqrt(np.abs(S))
+    zt=theta0
+    zs=S
+    zh=Z
+     
+    # compute volumic mass pure water at atm pressure
+    zr1= ( ( ( ( 6.536332e-9*zt-1.120083e-6 )*zt+1.001685e-4)*zt
+             -9.095290e-3 )*zt+6.793952e-2 )*zt+999.842594
+    # seawater volumic mass atm pressure
+    zr2= ( ( ( 5.3875e-9*zt-8.2467e-7 ) *zt+7.6438e-5 ) *zt
+           -4.0899e-3 ) *zt+0.824493
+    zr3= ( -1.6546e-6*zt+1.0227e-4 ) *zt-5.72466e-3
+    zr4= 4.8314e-4
+
+    # potential volumic mass (reference to the surface)
+    zrhop= ( zr4*zs + zr3*zsr + zr2 ) *zs + zr1
+
+    # add the compression terms
+    ze = ( -3.508914e-8*zt-1.248266e-8 ) *zt-2.595994e-6
+    zbw= (  1.296821e-6*zt-5.782165e-9 ) *zt+1.045941e-4
+    zb = zbw + ze * zs
+
+    zd = -2.042967e-2
+    zc =   (-7.267926e-5*zt+2.598241e-3 ) *zt+0.1571896
+    zaw= ( ( 5.939910e-6*zt+2.512549e-3 ) *zt-0.1028859 ) *zt - 4.721788
+    za = ( zd*zsr + zc ) *zs + zaw
+
+    zb1=   (-0.1909078*zt+7.390729 ) *zt-55.87545
+    za1= ( ( 2.326469e-3*zt+1.553190)*zt-65.00517 ) *zt+1044.077
+    zkw= ( ( (-1.361629e-4*zt-1.852732e-2 ) *zt-30.41638 )
+           *zt + 2098.925 ) *zt+190925.6
+    zk0= ( zb1*zsr + za1 )*zs + zkw
+
+    prd=  zrhop / (  1.0 - zh / ( zk0 - zh * ( za - zh * zb )))
+
+    return prd
+
+
+def density_meters_xr(theta0,s,z):
+    """In-situ density (volumic mass) using depth value
+    in meters.
+
+    Parameters
+    ----------
+    theta0 : xarray dataarray
+        potential temperature
+    s : xarray dataarray
+        salinity
+    z : xarray dataarray
+        depth (m)
+
+    Notes 
+    -----
+    We use  Jackett and McDougall (1995)'s [1]_ equation of state.
+    the in situ density is computed directly as a function of
+    potential temperature relative to the surface (theta0), salt 
+    and pressure (assuming no pressure variation
+    along geopotential surfaces, i.e. the pressure p in decibars
+    is approximated by the depth in meters.
+
+    with:
+
+    - depth                         z        meters
+
+    - potential temperature                   theta0        deg celsius
+
+    - salinity                      s        psu
+    
+
+    """
+
+    
+#    theta=_theta_xr(t,s,z)
+
+    # caution: depth (m) replaces pressure (db)        
+    rho=_density_xr(theta0,s,z)
+                    
+    return rho
--- oocgcm/oceanfuncs/eos/misc.py
+++ oocgcm/oceanfuncs/eos/misc.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python
 #
 """oocgcm.oceanfuncs.eos.misc
-Equation of state related functions that are not attached to a particular
+Equation of state-related functions that are not attached to a particular
 equation of state of sea water.
+
+Potential temperature is the 0-bar reference.
+
+
+
+Currently, functions are split into two families: one dealing with
+numpy arrays only (if useful for optimisation), the other dealing
+with  xarray dataarrays as input (if no optimisation needed).
+
+The list is as follows:
+
+- Numpy only:
+_spice : spiciness from theta0 and s
+_theta : theta0 from t,s,p
+_gamma_b73 : adiabatic lapse rate from Bryden (1973)    )     alternative code is a safer option
+_gamma_b73_2 : alternative code                         )
+
+- Xarray datarray input
+spice: spiciness from theta0 and s. Numpy calculation and output.
+_theta_xr: theta0 from t,s,p. Xarray only.
+_gamma_b73_xr : adiabatic lapse rate from Bryden (1973)    )     alternative code is a safer option
+_gamma_b73_2_xr : alternative code                         )
+
 
 """
 
 import numpy as np
 from numba import jit
 
-from ...core.utils import map_apply, _assert_are_compatible_dataarrays
+#from ...core.utils import map_apply, _assert_are_compatible_dataarrays
+from ...core.utils import *
+from matplotlib.pyplot import xkcd
+#from build.lib.oocgcm.parameters.physicalparameters import grav
+
 
 #---------------------- Functions for numpy arrays ----------------------------
 
@@ -24,8 +51,30 @@ _spice_coefs = \
         [-6.36e-010,    -1.309e-009,   6.048e-009, -1.1409e-009,  -6.676e-010]
     ])
 
-@jit
-def _spice(t,s):
+
+
+    
+_gamma_b73_coefs = \
+    np.array([
+        [ 
+         [ 0.35803e-001,     0.85258e-002,   -0.68360e-004,  0.66228e-006  ],
+         [ 0.18932e-002,     -0.42393e-004,    0.          ,  0.           ],
+         [ 0.,0.,0.,0.]
+        ],
+        [
+         [0.18741e-004,      -0.67795e-006,   0.87330e-008, -0.54481e-010 ],  
+         [-0.11351e-006,      0.27759e-008,   0.          ,  0.           ],
+         [ 0.,0.,0.,0.]
+        ],
+        [         
+         [-0.46206e-009,     0.18676e-010,   -0.21687e-012,  0.           ],
+         [0.,0.,0.,0.],
+         [0.,0.,0.,0.]
+        ]
+    ])
+
+#@jit
+def _spice(theta0,s):
     """Return spiciness.
 
     A state variable for characterizing water masses and their
@@ -33,7 +82,7 @@ def _spice(t,s):
 
     Parameters
     ----------
-    t : numpy.array
+    theta0 : numpy.array
         potential temperature (degC)
     s : numpy.array
         salinity (PSU)
@@ -74,8 +123,195 @@ def _spice(t,s):
         dtmp *= t
     return dspi
 
+def _theta(t,s,p):
+    """Return potential temperature (degC) at 0-bar level using pressure in
+    db.
+
+    Parameters
+    ----------
+    t : numpy.array
+        temperature (degC)
+    s : numpy.array
+        salinity (PSU)
+    p : numpy.array
+        pressure (db)
+
+    Returns
+    -------
+    theta : numpy.array
+        potential temperature
+
+    Notes
+    -----
+    The calculation follows Bryden (1973) for the adiabatic lapse rate and
+    Fofonoff (1977) for the integration to a reference level (i.e. a 
+    1-iterate Runge-Kutta 4 scheme)
+
+
+    References
+    ----------
+
+    """
+
+    r2=np.sqrt(2.)
+
+    # Adiabatic lapse rate (degC/db)
+    #g=lambda t,s,p: _gamma_b73(t,s,p)*0.001
+    g=lambda t,s,p: _gamma_b73_2(t,s,p)
+#
+#     # My script
+#     
+#     pr=0.
+#     dp= pr - p
+#     
+#     dt1 =   dp*g(t,  s,  p      )
+#     
+#     t1  =   t           +   0.5*dt1
+#     
+#     dt2 =   dp*g(t1, s,  p+0.5*dp)
+#     
+#     q1  =   dt1
+#     
+#     t2  =   t1          +   (1.-1./r2)*(dt2-q1)
+#     
+#     dt3 =   dp*g(t2, s,  p+0.5*dp)
+#     
+#     q2  =   (2.-r2)*dt2  +   (-2.+3./r2)*dt1
+#     
+#     t3  =   t2          +   (1.+1./r2)*(dt3-q2)
+#     
+#     dt4 =   dp*g(t3, s,  p+dp    )
+#     
+#     q3  =   (2.+r2)*dt3  +   (-2.-3./r2)*dt2
+#         
+#     theta = t3          +   (1./6.)*(dt4-2.*q3)
+
+    
+    # Fofonoff and Millard (1983)
+
+    pr=0.
+    h=pr-p
+    xk=h*g(t,s,p)
+    t=t+0.5*xk
+    q=xk
+    p=p+0.5*h
+    xk=h*g(t,s,p)
+    t=t+0.29289322*(xk-q)
+    q=0.58578644*xk+0.121320344*q
+    xk=h*g(t,s,p)
+    t=t+1.707106781*(xk-q)
+    q=3.424213562*xk-4.121320344*q
+    p=p+0.5*h
+    xk=h*g(t,s,p)
+    theta=t+(xk-2.0*q)/6.0
+
+    
+    print 'theta: '
+    print theta[0,0,0]
+    
+    return theta
+    
+#@jit
+def _gamma_b73(t,s,p):
+    """Return adiabatic lapse rate (degC/1000 db) using pressure 
+    in db.
+
+    Parameters
+    ----------
+    t : numpy.array
+        temperature (degC)
+    s : numpy.array
+        salinity (PSU)
+    p : numpy.array
+        pressure (db)
+
+    Returns
+    -------
+    gamma : numpy.array
+        adiabatic lapse rate
+
+    Notes
+    -----
+    The calculation follows Bryden (1973) polynomial.
+
+
+    References
+    ----------
+
+    """
+
+    salref = s - 35.
+
+    gamma    = 0.
+    
+    dp=1.
+    for ji in range(3):
+          dsal=1.
+          for jj in range(3):
+              dtmp = 1.
+              for jk in range(4):
+                  gamma = gamma +   _gamma_b73_coefs[ji,jj,jk] * dtmp * dsal * dp
+                  dtmp *= t
+              dsal *= salref
+          dp *= p
+          
+    print 'gamma: '      
+    print gamma[0,0,0]     
+
+    return gamma
+
+def _gamma_b73_2(t,s,p):
+    """Adiabatic lapse rate (deg C/dBar).
+ 
+    Adiabatic lapse rate (deg C/dBar) from salinity (psu), 
+    temperature (deg C) and pressure (dbar)
+
+    Parameters
+    ----------
+    t : numpy.array
+        temperature (deg C)
+    s : numpy.array
+        salinity (psu)
+    p : numpy.array
+        pressure (dbar)
+
+
+    Notes 
+    -----
+    
+    The script is drawn from Fofonoff and Millard (1983).
+    The calculation follows Bryden (1973) polynomial.
+
+ 
+    References
+    ----------
+
+
+    """ 
+    #
+    ds = s - 35.0
+    atg = ((-2.1687e-16 * t + 1.8676e-14) * t - 4.6206e-13) * p * p
+    atg+= (2.7759e-12 * t - 1.1351e-10 ) * ds * p
+    atg+= (((-5.4481e-14 * t + 8.7330e-12) * t - 6.7795e-10) * t + 1.8741e-8) * p
+    atg+= (-4.2393e-8 * t + 1.8932e-6 ) * ds
+    atg+= ((6.6228e-10 * t - 6.8360e-8) * t + 8.5258e-6) * t + 3.5803e-5
+    
+    print atg[0,0].values
+    
+    return atg
+ 
+
+
+
+
 
 #------------------ Wrapped functions for dataarrays ---------------------------
+
+
+
+
+
+
 def spice(t,s):
     """Return spiciness.
 
@@ -109,3 +345,178 @@ def spice(t,s):
     .. [2] Jackett and McDougall, Deep Sea Research, 32A, 1195-1208, 1985.
     """
     pass
+
+
+def _theta_xr(t,s,p):
+    """Return potential temperature (degC) at 0-bar level using pressure in
+    db.
+
+    Parameters
+    ----------
+    t : xarray dataarray
+        temperature (degC)
+    s : xarray dataarray
+        salinity (PSU)
+    p : xarray dataarray
+        pressure (db)
+
+    Returns
+    -------
+    theta : xarray dataarray
+        potential temperature
+
+    Notes
+    -----
+    The calculation follows Bryden (1973) for the adiabatic lapse rate and
+    Fofonoff (1977) for the integration to a reference level (i.e. a 
+    1-iterate Runge-Kutta 4 scheme).
+
+
+    References
+    ----------
+
+    """
+
+    r2=np.sqrt(2)
+
+    # Adiabatic lapse rate (degC/db)
+    g=lambda t,s,p:  _gamma_b73_2_xr(t,s,p)
+    #g=lambda t,s,p: _gamma_b73_xr(t,s,p)*0.001
+
+#     #My script    
+#     pr=0.
+#     dp= pr - p
+#     
+#     dt1 =   dp*g(t,  s,  p      )
+#     
+#     t1  =   t           +   0.5*dt1
+#     
+#     dt2 =   dp*g(t1, s,  p+0.5*dp)
+#     
+#     q1  =   dt1
+#     
+#     t2  =   t1          +   (1.-1./r2)*(dt2-q1)
+#     
+#     dt3 =   dp*g(t2, s,  p+0.5*dp)
+#     
+#     q2  =   (2.-r2)*dt2  +   (-2.+3./r2)*dt1
+#     
+#     t3  =   t2          +   (1.+1./r2)*(dt3-q2)
+#     
+#     dt4 =   dp*g(t3, s,  p+dp    )
+#     
+#     q3  =   (2.+r2)*dt3  +   (-2.-3./r2)*dt2
+#         
+#     theta = t3          +   (1./6.)*(dt4-2.*q3)
+#
+
+    # Fofonoff and Millard (1983)
+
+    pr=0.
+    h=pr-p
+    xk=h*g(t,s,p)
+    t=t+0.5*xk
+    q=xk
+    p=p+0.5*h
+    xk=h*g(t,s,p)
+    t=t+0.29289322*(xk-q)
+    q=0.58578644*xk+0.121320344*q
+    xk=h*g(t,s,p)
+    t=t+1.707106781*(xk-q)
+    q=3.424213562*xk-4.121320344*q
+    p=p+0.5*h
+    xk=h*g(t,s,p)
+    theta=t+(xk-2.0*q)/6.0
+    
+    
+    return theta
+    
+
+def _gamma_b73_xr(t,s,p):
+    """Return adiabatic lapse rate (degC/1000 db) using pressure 
+    in db.
+
+    Parameters
+    ----------
+    t : xarray dataarray
+        temperature (degC)
+    s : xarray dataarray
+        salinity (PSU)
+    p : xarray dataarray
+        pressure (db)
+
+    Returns
+    -------
+    gamma : xarray dataarray
+        adiabatic lapse rate
+
+    Notes
+    -----
+
+    The calculation follows Bryden (1973) polynomial. 
+
+
+    References
+    ----------
+
+    """
+
+    salref = s - 35.
+
+    gamma    = 0.
+    
+    dp=1.
+    for ji in range(3):
+          dsal=1.
+          for jj in range(3):
+              dtmp = 1.
+              for jk in range(4):
+                  gamma = gamma +   _gamma_b73_coefs[ji,jj,jk] * dtmp * dsal * dp
+                  dtmp *= t
+              dsal *= salref
+          dp *= p 
+
+    print gamma[0,0].values
+    
+    return gamma
+
+
+def _gamma_b73_2_xr(t,s,p):
+    """Adiabatic lapse rate (deg C/dBar).
+ 
+    Adiabatic lapse rate (deg C/dBar) from salinity (psu), 
+    temperature (deg C) and pressure (dbar)
+
+    Parameters
+    ----------
+    t : xarray dataarray
+        temperature (deg C)
+    s : xarray dataarray
+        salinity (psu)
+    p : xarray dataarray
+        pressure (dbar)
+
+
+    Notes 
+    -----
+    
+    The script is drawn from Fofonoff and Millard (1983).
+    The calculation follows Bryden (1973) polynomial.
+
+ 
+    References
+    ----------
+
+
+    """ 
+    #
+    ds = s - 35.0
+    atg = ((-2.1687e-16 * t + 1.8676e-14) * t - 4.6206e-13) * p * p
+    atg+= (2.7759e-12 * t - 1.1351e-10 ) * ds * p
+    atg+= (((-5.4481e-14 * t + 8.7330e-12) * t - 6.7795e-10) * t + 1.8741e-8) * p
+    atg+= (-4.2393e-8 * t + 1.8932e-6 ) * ds
+    atg+= ((6.6228e-10 * t - 6.8360e-8) * t + 8.5258e-6) * t + 3.5803e-5
+    
+    print atg[0,0].values
+    
+    return atg
